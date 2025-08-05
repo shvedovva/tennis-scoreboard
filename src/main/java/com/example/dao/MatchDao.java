5458af180b606f14970292bf3236b6d931039d89
Vadim Shvedov <shvedovva@gmail.com>
2025-08-05 12:54:14 +0300

    add method to service

--- src/main/java/com/example/dao/MatchDao.java
+++ src/main/java/com/example/dao/MatchDao.java
@@ -9,23 +9,35 @@ import org.hibernate.query.Query;
 import java.util.List;
 
 public class MatchDao {
-    public Match save(Match match){
+    public Match save(Match match) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.save(match);
             transaction.commit();
             return match;
         } catch (Exception e) {
-            if (transaction != null){
+            if (transaction != null) {
                 transaction.rollback();
             }
             throw new RuntimeException("Error saving match", e);
         }
     }
 
-    public List<Match> findAll(String playerName, int page, int pageSize){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+    public List<Match> findAll(int page, int pageSize) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Match> query = session.createQuery("FROM Match m ORDER BY m.id DESC", Match.class);
+            query.setFirstResult((page - 1) * pageSize);
+            query.setMaxResults(pageSize);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Error finding matches", e);
+        }
+
+    }
+
+    public List<Match> findByPlayerName(String playerName, int page, int pageSize) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Match> query = session.createQuery("FROM Match m WHERE m.player1.name = :playerName OR m.player2.name = :playerName ORDER BY m.id DESC", Match.class);
             query.setParameter("playerName", playerName);
             query.setFirstResult((page - 1) * pageSize);
@@ -36,17 +48,17 @@ public class MatchDao {
         }
     }
 
-    public long countAll(){
+    public long countAll() {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Long> query = session.createQuery("SELECT COUNT(m) FROM Match m", Long.class);
             return query.getSingleResult();
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Error counting matches", e);
         }
     }
 
     public long countByPlayerName(String playerName) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Long> query = session.createQuery(
                     "SELECT COUNT(m) FROM Match m WHERE m.player1.name = :playerName OR m.player2.name = :playerName", Long.class
             );
--- src/main/java/com/example/service/FinishedMatchesPersistenceService.java
+++ src/main/java/com/example/service/FinishedMatchesPersistenceService.java
@@ -4,6 +4,8 @@ import com.example.dao.MatchDao;
 import com.example.model.Match;
 import com.example.model.MatchScore;
 
+import java.util.List;
+
 public class FinishedMatchesPersistenceService {
     private final MatchDao matchDao = new MatchDao();
 
@@ -16,5 +18,19 @@ public class FinishedMatchesPersistenceService {
                                 ,matchScore.getWinner());
         return matchDao.save(match);
     }
+     public List<Match> getMatches(int page, int pageSize){
+        return matchDao.findAll(page, pageSize);
+     }
+
+     public List<Match> getMatchesByPlayerName(String playerName, int page, int pageSize){
+        return matchDao.findByPlayerName(playerName, page,pageSize);
+     }
+
+     public long getTotalMatchesCount(){
+        return matchDao.countAll();
+     }
+     public  long getTotalMatchesCountByPlayerName(String playerName){
+        return matchDao.countByPlayerName(playerName);
+     }
 
 }
