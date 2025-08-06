0bd365ca7328b52d30b09438516002533bd1ac9e
Vadim Shvedov <shvedovva@gmail.com>
2025-08-06 07:45:20 +0300

    refactr to jakarta

--- src/main/java/com/example/util/AppInitializer.java
+++ src/main/java/com/example/util/AppInitializer.java
@@ -1,7 +1,7 @@
 package com.example.util;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
 
 public class AppInitializer implements ServletContextListener {
 
